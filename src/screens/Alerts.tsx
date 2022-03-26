964f24e696372cda3d8283c4c38927ab686db455
Jaime Cabrera <jycabrerap@unl.edu.ec>
2022-03-26 09:25:28 -0500

    add alert component

---
+++ src/screens/Alerts.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Button, Text, View, Alert} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {globalStyles} from '../theme/appTheme';
+
+export const Alerts = () => {
+  const showAlert = () => {
+    Alert.alert(
+      'Titulo',
+      'Alert content',
+      [
+        {
+          text: 'Cancelar',
+          style: 'default',
+        },
+        {
+          text: 'Ok',
+          style: 'cancel',
+        },
+      ],
+      {
+        // esyto para cancelar si le doy click
+        cancelable: true,
+      },
+    );
+  };
+
+  return (
+    <View style={globalStyles.container}>
+      <HeaderTitle title="Alert" />
+      <Button title="Mostrar Alerta" onPress={() => showAlert()} />
+    </View>
+  );
+};
